6934d9117271245d74c696a5814df659b19a2058
forSplinter* <mugangamathys24@gmail.com>
2025-05-05 16:13:21 +0200

    Fix Kafka broker image in Docker Compose and update Alpaca ingestion script for timezone handling

--- producers/alpaca_ingestion.py
+++ producers/alpaca_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import json
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from kafka import KafkaProducer
 from alpaca.data import StockHistoricalDataClient, StockTradesRequest
 
@@ -14,11 +14,16 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "alpaca_raw_stream")
 SYMBOLS = ["AAPL", "MSFT", "GOOGL", "TSLA", "NVDA", "AMZN"]
 INTERVAL_SECONDS = 5
 
+if not ALPACA_API_KEY or not ALPACA_SECRET_KEY:
+    raise ValueError("Credentials not set")
+
 # Initialize client & producer
 client = StockHistoricalDataClient(ALPACA_API_KEY, ALPACA_SECRET_KEY)
+
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BROKER,
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    retries=5,
 )
 
 def serialize_trade(symbol, trade):
@@ -35,49 +40,69 @@ def serialize_trade(symbol, trade):
     }
 
 def marketOpen():
-    now = datetime.utcnow()
-    return now.hour >= 14 and now.hour < 21  # 14h30–21h00 UTC approx
+    print("Checking if market is open...")
+    now = datetime.now(timezone.utc)
+    return 14 <= now.hour < 21 # 14h30–21h00 UTC approx
+
 
 def runStream():
     print("Starting Alpaca producer stream...")
-    last_fetch = datetime.utcnow() - timedelta(seconds=INTERVAL_SECONDS)
+    now = datetime.now(timezone.utc) - timedelta(days=4)
+    start = now.replace(hour=18, minute=0, second=0, microsecond=0)
+    end = now.replace(hour=18, minute=5, second=0, microsecond=0)
 
-    while marketOpen():
-        now = datetime.utcnow()
-        print(f"[{now}] Fetching trades since {last_fetch}")
-        for symbol in SYMBOLS:
-            try:
-                request = StockTradesRequest(
-                    symbol_or_symbols=symbol,
-                    start=last_fetch,
-                    end=now
-                )
-                result = client.get_stock_trades(request)
-                trades = result.data.get(symbol, [])
-                print(f"{len(trades)} trades for {symbol}")
-                for trade in trades:
-                    serialized = serialize_trade(symbol, trade)
-                    producer.send(KAFKA_TOPIC, value=serialized)
-            except Exception as e:
-                print(f"Error fetching trades for {symbol}: {e}")
+    print(f"[{datetime.now()}]Fetching trades from {start} to {end}")
 
-        last_fetch = now
-        producer.flush()
-        time.sleep(INTERVAL_SECONDS)
+    for symbol in SYMBOLS:
+        try:
+            request = StockTradesRequest(
+                symbol_or_symbols=symbol,
+                start=start,
+                end=end
+            )
+            result = client.get_stock_trades(request)
+            trades = result.data.get(symbol, [])
+            print(f"{len(trades)} trades for {symbol}")
+            for trade in trades:
+                serialized = serialize_trade(symbol, trade)
+                producer.send(KAFKA_TOPIC, value=serialized)
+        except Exception as e:
+            print(f"Error fetching trades for {symbol}: {e}")
+
+    producer.flush()
+    print("Finished fetching trades.")
+
+
+    # Uncomment the following lines to run the stream continuously
+
+# def runStream():
+#     print("Starting Alpaca producer stream...")
+#     last_fetch = datetime.now(timezone.utc) - timedelta(seconds=INTERVAL_SECONDS)
+
+#     while marketOpen():
+#         now = datetime.now(timezone.utc)
+#         print(f"[{now}] Fetching trades since {last_fetch}")
+#         for symbol in SYMBOLS:
+#             try:
+#                 request = StockTradesRequest(
+#                     symbol_or_symbols=symbol,
+#                     start=last_fetch,
+#                     end=now
+#                 )
+#                 result = client.get_stock_trades(request)
+#                 trades = result.data.get(symbol, [])
+#                 print(f"{len(trades)} trades for {symbol}")
+#                 for trade in trades:
+#                     serialized = serialize_trade(symbol, trade)
+#                     producer.send(KAFKA_TOPIC, value=serialized)
+#             except Exception as e:
+#                 print(f"Error fetching trades for {symbol}: {e}")
+
+#         last_fetch = now
+#         producer.flush()
+#         time.sleep(INTERVAL_SECONDS)
 
     print("Market closed. Stopping stream.")
 
 if __name__ == "__main__":
     runStream()
-
-# request_params = StockTradesRequest(
-#     symbol_or_symbols= "AAPL",
-#     start=datetime(2024, 1, 30, 14, 30),
-#     end=datetime(2024, 1, 30, 14, 45)
-# )
-
-# trades = data_client.get_stock_trades(request_params)
-
-# for trade in trades.data["AAPL"]:
-#     print(trade)
-#     break
